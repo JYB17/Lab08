ed286013db17f97cc5837f138ac705f0dd095e90
JYB17 <jyback99@naver.com>
2018-04-03 10:58:58 +0900

    lab08

--- src/main/java/edu/handong/csee/java/lab08/Overload.java
+++ src/main/java/edu/handong/csee/java/lab08/Overload.java
@@ -12,17 +12,15 @@ public class Overload {
 		System.out.println("average2 = " + average2);
 		System.out.println("average3 = " + average3);
 	}
-	
-	public static double getAverage(double first, double second) {
-		return (first+second)/2;
-	}
-	
-	public static double getAverage(double first, double second, double third) {
-		return (first+second+third)/3;
-	}
-	
-	public static char getAverage(char first, char second) {
-		return (char)(((int)first + (int)second)/2);
-	}
-
+		public static double getAverage(double first, double second) {
+			return (first+second)/2;
+		}
+		
+		public static double getAverage(double first, double second, double third) {
+			return (first+second+third)/3;
+		}
+		
+		public static char getAverage(char first, char second) {
+			return (char)(((int)first+(int)second)/2);
+		}
 }
